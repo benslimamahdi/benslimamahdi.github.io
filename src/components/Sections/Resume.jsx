c83b719d387622459c054be14c719caa4e85ea32
Mahdi <benslima.mahdi@gmail.com>
2025-12-15 09:57:08 +0100

    Refactor: Separate content from code (JSON data & public images)

--- src/components/Sections/Resume.jsx
+++ src/components/Sections/Resume.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import SectionWrapper from '../Layout/SectionWrapper';
 import './Resume.css';
 
+import resumeData from '../../data/resume.json';
+
 const Resume = () => {
     return (
         <SectionWrapper id="resume" className="resume-section">
@@ -13,65 +15,40 @@ const Resume = () => {
                 <div className="resume-column">
                     <h3><i className="las la-briefcase"></i> Work Experience</h3>
 
-                    <div className="resume-item">
-                        <span className="date">Feb 2025 - Sep 2025</span>
-                        <h4>DevOps Intern</h4>
-                        <span className="location">Oneex, Clermont Ferrand, France</span>
-                        <p>
-                            Architected and automated multi-zone cloud infrastructure using Terraform, Ansible, and ArgoCD.
-                            Implemented proactive monitoring and alerting systems (Grafana, Prometheus).
-                        </p>
-                    </div>
-
-                    <div className="resume-item">
-                        <span className="date">Jun 2024 - Jul 2024</span>
-                        <h4>DevOps Intern</h4>
-                        <span className="location">PerfectSoft, Sfax, Tunisia</span>
-                        <p>
-                            Deployed Azure-based monitoring stack (Prometheus, Grafana, Loki).
-                            Implemented Infrastructure-as-Code (IaC) using Terraform.
-                        </p>
-                    </div>
-
-                    <div className="resume-item">
-                        <span className="date">Jun 2023 - Aug 2023</span>
-                        <h4>DevOps Intern</h4>
-                        <span className="location">DataSphera, Sfax, Tunisia</span>
-                        <p>
-                            Developed and deployed highly efficient CI/CD pipelines in GitLab CI for EKS clusters.
-                            Integrated security scanning and compliance tools (Trivy, SonarQube).
-                        </p>
-                    </div>
+                    {resumeData.experience.map((item) => (
+                        <div key={item.id} className="resume-item">
+                            <span className="date">{item.date}</span>
+                            <h4>{item.title}</h4>
+                            <span className="location">{item.company}, {item.location}</span>
+                            {item.description && (
+                                <p>{item.description}</p>
+                            )}
+                        </div>
+                    ))}
                 </div>
 
                 {/* Education & Leadership Column */}
                 <div className="resume-column">
                     <h3><i className="las la-university"></i> Education</h3>
-                    <div className="resume-item">
-                        <span className="date">2020 - 2025</span>
-                        <h4>National Engineering Diploma in Data Engineering</h4>
-                        <span className="location">Faculty of Sciences, University of Sfax</span>
-                        <p> Specialized in Data Engineering and DevOps practices.</p>
-                    </div>
+                    {resumeData.education.map((item) => (
+                        <div key={item.id} className="resume-item">
+                            <span className="date">{item.date}</span>
+                            <h4>{item.title}</h4>
+                            <span className="location">{item.institution}</span>
+                            {item.description && (
+                                <p>{item.description}</p>
+                            )}
+                        </div>
+                    ))}
 
                     <h3 style={{ marginTop: '40px' }}><i className="las la-star"></i> Leadership</h3>
-                    <div className="resume-item">
-                        <span className="date">2025 - 2026</span>
-                        <h4>Young Professionals Coordinator</h4>
-                        <span className="location">IEEE Future Networks Forum</span>
-                    </div>
-
-                    <div className="resume-item">
-                        <span className="date">2025 - 2026</span>
-                        <h4>Co-Founder & Operations Lead</h4>
-                        <span className="location">DevAura (Freelance)</span>
-                    </div>
-
-                    <div className="resume-item">
-                        <span className="date">2024 - 2025</span>
-                        <h4>Student Branch Chair</h4>
-                        <span className="location">IEEE Faculty of Sciences of Sfax</span>
-                    </div>
+                    {resumeData.leadership.map((item) => (
+                        <div key={item.id} className="resume-item">
+                            <span className="date">{item.date}</span>
+                            <h4>{item.title}</h4>
+                            <span className="location">{item.organization}</span>
+                        </div>
+                    ))}
                 </div>
             </div>
         </SectionWrapper>
